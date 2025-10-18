c277e8f6cbe821295f1941c6237a3ad568dd33c2
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-10-17 17:39:43 -0700

    feat(Data): add updates to migrations, seeds, and database

---
+++ Sahara.API/Data/Migrations/20251018002944_AddStatusToAdmin.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sahara.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusToAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Admins");
+        }
+    }
+}
