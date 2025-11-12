d78e539cff1e7154784a7f64392967353a78ca04
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-11-12 04:25:10 -0800

    feat(contexts): add ApplyConfigurationsFromAssembly method to apply entity data configs to model

--- Sahara.API/Data/Contexts/AppDbContext.cs
+++ Sahara.API/Data/Contexts/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Sahara.API.Entities;
+using System.Reflection;
 
 namespace Sahara.API.Data.Contexts
 {
@@ -21,128 +22,10 @@ namespace Sahara.API.Data.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Query filter to exclude soft-deleted records from all queries by default checking IsDeleted property.
-            modelBuilder.Entity<User>()
-                .HasQueryFilter(u => !u.IsDeleted);
+            // Scans the current assembly for all IEntityTypeConfiguration<T> implementations and applies them to the model.
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
-            modelBuilder.Entity<Customer>()
-                .HasQueryFilter(c => !c.IsDeleted);
-
-            modelBuilder.Entity<Vendor>()
-                .HasQueryFilter(v => !v.IsDeleted);
-
-            modelBuilder.Entity<Admin>()
-                .HasQueryFilter(a => !a.IsDeleted);
-
-            modelBuilder.Entity<Product>()
-                .HasQueryFilter(p => !p.IsDeleted);
-
-            modelBuilder.Entity<Category>()
-                .HasQueryFilter(c => !c.IsDeleted);
-
-            // Prevents cascade delete of parent entity affecting child entities.
-            // Hard-delete will not be used, data will only be soft-deleted.
-            modelBuilder.Entity<Product>()
-                .HasOne(p => p.Vendor)
-                .WithMany(v => v.Products)
-                .HasForeignKey(p => p.VendorId)
-                .OnDelete(DeleteBehavior.Restrict)
-                .IsRequired(false);
-
-            modelBuilder.Entity<Product>()
-                .HasOne(p => p.Category)
-                .WithMany(c => c.Products)
-                .HasForeignKey(p => p.CategoryId)
-                .OnDelete(DeleteBehavior.Restrict)
-                .IsRequired(false);
-
-            modelBuilder.Entity<OrderItem>()
-                .HasOne(oi => oi.Product)
-                .WithMany()
-                .HasForeignKey(oi => oi.ProductId)
-                .OnDelete(DeleteBehavior.Restrict)
-                .IsRequired(false);
-
-            modelBuilder.Entity<OrderItem>()
-                .HasOne(oi => oi.Order)
-                .WithMany(o => o.OrderItems)
-                .HasForeignKey(oi => oi.OrderId)
-                .OnDelete(DeleteBehavior.Restrict)
-                .IsRequired(false);
-
-            modelBuilder.Entity<Payment>()
-                .HasOne(p => p.Order)
-                .WithOne(p => p.Payment)
-                .HasForeignKey<Payment>(p => p.OrderId)
-                .OnDelete(DeleteBehavior.Restrict)
-                .IsRequired(false);
-
-            modelBuilder.Entity<Vendor>()
-                .HasOne(v => v.User)
-                .WithOne(u => u.Vendor)
-                .HasForeignKey<Vendor>(v => v.UserId)
-                .OnDelete(DeleteBehavior.Restrict)
-                .IsRequired(false);
-
-            modelBuilder.Entity<Customer>()
-                .HasOne(c => c.User)
-                .WithOne(u => u.Customer)
-                .HasForeignKey<Customer>(c => c.UserId)
-                .OnDelete(DeleteBehavior.Restrict)
-                .IsRequired(false);
-
-            modelBuilder.Entity<Order>()
-                .HasOne(o => o.Customer)
-                .WithMany(c => c.Orders)
-                .HasForeignKey(o => o.CustomerId)
-                .OnDelete(DeleteBehavior.Restrict)
-                .IsRequired(false);
-
-            // Decimal precision configuration.
-            modelBuilder.Entity<Order>()
-                .Property(o => o.TotalAmount)
-                .HasPrecision(18, 2);
-
-            modelBuilder.Entity<OrderItem>()
-                .Property(oi => oi.UnitPrice)
-                .HasPrecision(18, 2);
-
-            modelBuilder.Entity<Product>()
-                .Property(p => p.Price)
-                .HasPrecision(18, 2);
-
-            modelBuilder.Entity<Payment>()
-                .Property(p => p.Amount)
-                .HasPrecision(18, 2);
-
-            // String conversion for enum int type to strings in sql.
-            modelBuilder.Entity<User>()
-                .Property(u => u.Role)
-                .HasConversion<string>();
-
-            modelBuilder.Entity<Admin>()
-                .Property(a => a.Status)
-                .HasConversion<string>();
-
-            modelBuilder.Entity<Customer>()
-                .Property(c => c.Status)
-                .HasConversion<string>();
-
-            modelBuilder.Entity<Vendor>()
-                .Property(v => v.Status)
-                .HasConversion<string>();
-
-            modelBuilder.Entity<Order>()
-                .Property(o => o.Status)
-                .HasConversion<string>();
-
-            modelBuilder.Entity<Payment>()
-                .Property(p => p.Status)
-                .HasConversion<string>();
-
-            modelBuilder.Entity<Product>()
-                .Property(p => p.Status)
-                .HasConversion<string>();
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
