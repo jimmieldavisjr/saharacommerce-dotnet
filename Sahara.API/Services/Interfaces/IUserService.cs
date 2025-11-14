9398f713af8c3e75cae9b588c0ee06a3b5500aae
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-11-14 14:26:01 -0800

    feat(user): add IUserService

---
+++ Sahara.API/Services/Interfaces/IUserService.cs
@@ -0,0 +1,24 @@
+﻿using Sahara.API.Entities;
+
+namespace Sahara.API.Services.Interfaces
+{
+    /// <summary>
+    /// Defines operations for managing user profiles.
+    /// </summary>
+    public interface IUserService
+    {
+        /// <summary>
+        /// Updates the user's profile.
+        /// </summary>
+        /// <param name="user">The user entity with optional updated values.</param>
+        /// <returns>The updated user entity.</returns>
+        Task<User> UpdateProfile(User user);
+
+        /// <summary>
+        /// Soft deletes the user marking it as deleted (IsDeleted = true).
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>The soft deleted user entity with updated (IsDeleted) property.</returns>
+        Task<User> SoftDelete(User user);
+    }
+}
