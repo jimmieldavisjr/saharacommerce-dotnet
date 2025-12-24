329dbf1c420f66ef3131965ed56950a9c9a6eb8f
Jimmie Davis <life@jimmieldavisjr.com>
2025-12-24 05:21:49 -0800

    wip: adding DbContext - Identity Infrastructure

---
+++ src/Modules/Identity/Sahara.Modules.Identity.Infrastructure/Persistence/Context/DbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sahara.Modules.Identity.Infrastructure.Persistence.Context
+{
+    internal class IdentityDbContext
+    {
+    }
+}
---
+++ src/Modules/Identity/Sahara.Modules.Identity.Infrastructure/Registration/IdentityInfrastructureRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sahara.Modules.Identity.Infrastructure.Registration
+{
+    public static class IdentityInfrastructureRegistration
+    {
+        public static IServiceCollection AddIdentityInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+        }
+    }
+}
