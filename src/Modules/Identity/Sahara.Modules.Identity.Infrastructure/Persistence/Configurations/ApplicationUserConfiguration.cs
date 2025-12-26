085938e8c5774530ab7d0922e1a503232639462c
Jimmie Davis <life@jimmieldavisjr.com>
2025-12-26 00:07:08 -0800

    feat(identity): add EF Core configurations for ApplicationUser and ApplicationRole

---
+++ src/Modules/Identity/Sahara.Modules.Identity.Infrastructure/Persistence/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sahara.Modules.Identity.Infrastructure.Persistence.Configurations
+{
+    internal class ApplicationUserConfiguration
+    {
+    }
+}
