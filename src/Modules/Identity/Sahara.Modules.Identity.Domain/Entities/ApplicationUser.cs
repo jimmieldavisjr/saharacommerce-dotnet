3842596387c87accbdc282a94ed5288c4e6a5468
Jimmie Davis <life@jimmieldavisjr.com>
2026-01-09 16:23:41 -0800

    feature(identity/domain): complete ApplicationUser Entity with Anemic moodel design

--- src/Modules/Identity/Sahara.Modules.Identity.Domain/Entities/ApplicationUser.cs
+++ src/Modules/Identity/Sahara.Modules.Identity.Domain/Entities/ApplicationUser.cs
@@ -5,7 +5,16 @@ using System.Text;
 
 namespace Sahara.Modules.Identity.Domain.Entities
 {
-    public class ApplicationUser : IdentityUser<Guid>
+    public sealed class ApplicationUser : IdentityUser<Guid>
     {
+        // Lifecycle flags
+        public bool IsDeleted { get; set; }
+        public bool IsDisabled { get; set; }
+
+        // Operational Auditing
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset? LastLoginAt { get; set; }
+        public DateTimeOffset? DeletedAt { get; set; }
+        public DateTimeOffset? DisabledAt { get; set; }
     }
 } 
