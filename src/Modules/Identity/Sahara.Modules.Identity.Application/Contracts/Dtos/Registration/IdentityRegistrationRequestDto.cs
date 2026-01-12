717aa101a36101048f112c9ff4fc32c8ff19bf92
Jimmie Davis <life@jimmieldavisjr.com>
2026-01-12 03:03:30 -0800

    feat(identity/application): add IdentityRegistrationRequestDto for service implementation

---
+++ src/Modules/Identity/Sahara.Modules.Identity.Application/Contracts/Dtos/Registration/IdentityRegistrationRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sahara.Modules.Identity.Application.Contracts.Dtos.Registration
+{
+    internal class IdentityRegistrationRequestDto
+    {
+    }
+}
--- src/Modules/Identity/Sahara.Modules.Identity.Application/Contracts/Dtos/Registration/IdentityRegistrationResultDto.cs
+++ src/Modules/Identity/Sahara.Modules.Identity.Application/Contracts/Dtos/Registration/IdentityRegistrationResultDto.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Sahara.Modules.Identity.Application.Contracts.Dtos
+namespace Sahara.Modules.Identity.Application.Contracts.Dtos.Registration
 {
     internal class IdentityRegistrationResultDto
     {
